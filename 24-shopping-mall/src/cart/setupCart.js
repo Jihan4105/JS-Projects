64a9dc6e080e823c6f4c3cdf94de49a14d22f75d
Jihan4105 <wlgks6979@naver.com>
2024-12-10 09:40:22 +0900

    2024-12-10 Description Added

--- 24-shopping-mall/src/cart/setupCart.js
+++ 24-shopping-mall/src/cart/setupCart.js
@@ -15,6 +15,7 @@ const cartTotalDOM = getElement(".cart-total")
 
 let cart = getStorageItem("cart")
 
+//추가될 cartItem의 id를 받아서 추가하거나 증가시켜주는 함수.
 export const addToCart = (id) => {
   let item = cart.find((cartItem) => cartItem.id === id)
   
@@ -36,6 +37,7 @@ export const addToCart = (id) => {
   openCart()
 }
 
+//아이템 양을 하나 늘려서 cart변수에 update 해주는 함수.
 function increaseAmount(id) {
   let newAmount 
   cart = cart.map((cartItem) => {
@@ -48,6 +50,7 @@ function increaseAmount(id) {
   return newAmount
 }
 
+//아이템의 양을 하나 줄여서 cart변수에 update 해주는 함수.
 function decreaseAmount(id) {
   let newAmount
   cart = cart.map((cartItem) => {
@@ -60,7 +63,7 @@ function decreaseAmount(id) {
   return newAmount
 }
 
-//바뀐 선택한 전체 아이템의 갯수를 업데이트해줌
+//바뀐 선택한 전체 아이템의 갯수를 Navbar의 cart-icon 위의 span DOM 업데이트 해줌.
 function displayCartItemCount() {
   const amount = cart.reduce((total, cartItem) => {
     return (total += cartItem.amount)
@@ -69,7 +72,7 @@ function displayCartItemCount() {
   cartItemCountDOM.textContent = amount;
 }
 
-//바뀐 선택한 전체 아이템의 총값을 업데이트해줌
+//바뀐 선택한 전체 아이템의 총값을 DOM에 업데이트해줌
 function displayCartTotal() {
   let total = cart.reduce((total, cartItem) => {
     return (total += cartItem.price * cartItem.amount)
@@ -78,16 +81,19 @@ function displayCartTotal() {
   cartTotalDOM.textContent = `Total : ${formatPrice(total)}`
 }
 
+//바뀐 cart를 실제로 HTML에 뿌려주는 함수인 addToCartDOM을 호출하는 함수
 function displayCartItemsDOM() {
   cart.forEach((cartItem) => {
     addToCartDOM(cartItem)
   })
 }
 
+//id를 받아서 같은 id인 아이템을 cart 목록에서 없애주는 함수
 function removeItem(id) {
   cart = cart.filter((cartItem) => cartItem.id !== id)
 }
 
+//cartItem들의 증가, 감소, 제거 버튼의 eventHandler를 정의해주는 함수.
 function setupCartFunctionality() {
   cartItemsDOM.addEventListener("click", function (e) {
     const element = e.target
@@ -122,10 +128,18 @@ function setupCartFunctionality() {
   })
 }
 
+
 const init = () => {
+  //Navbar의 cart span 업데이트. 
   displayCartItemCount()
+
+  //보이지 않는 cart Sidebar의 Total 업데이트 => 그래야 전개했을때 저장된 total 값이 보일 수 있음.
   displayCartTotal()
+
+  //보이지 않는 cart Sidebar에 items DOM 뿌리기 => 그래야 전개했을때 저장된 item들이 보일 수 있음.
   displayCartItemsDOM()
+
+  //보이지 않는 cart Sidebar의 증가, 감소, 제거 버튼에 eventHandler를 달아줌.
   setupCartFunctionality()
 }
 
